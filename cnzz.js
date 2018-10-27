4ce712b55992376eb4b74fba11589b2284212696
Russell <42051421+ShowKingzw@users.noreply.github.com>
2018-10-28 02:13:41 +0800

    Add files via upload

---
+++ cnzz.js
@@ -0,0 +1,10 @@
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1000306544'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s5.cnzz.com/z_stat.php%3Fid%3D1000306544%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
+
+document.getElementById('cnzz_stat_icon_1000306544').style.display='none';
+
+window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"slide":{"type":"slide","bdImg":"6","bdPos":"left","bdTop":"100"},"image":{"viewList":["qzone","tsina","tqq","renren","weixin"],"viewText":"鍒嗕韩鍒帮細","viewSize":"16"},"selectShare":{"bdContainerClass":null,"bdSelectMiniList":["qzone","tsina","tqq","renren","weixin"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
+
+
+document.write('<div style="display: block; position: fixed; bottom: 0px; font-size: 20px;font-weight: bold;   background: #3b8de3; height: 50px; line-height: 50px; width: 100%;z-index: 1000000;"><a href="/" style="color:#fff; text-decoration: none; display: inline-block; text-align: center; width: 50%; border-right: #fff solid 1px;"></a><a href="/texiao.html?type=texiao" style="color:#fff; text-decoration: none; text-align: center; display: inline-block; width: 45%"></a></div>');
+
+document.write('<a style="position: fixed; right: 0px; top: 200px; z-index: 1000000;" target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=291429687&site=qq&menu=yes"><img border="0" src="/images/qq.gif" alt="" title=""/></a>');
